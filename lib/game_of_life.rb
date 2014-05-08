58d63d060ad27d1ba3abb4b642879e2e180a8978
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-05-07 21:55:29 -0400

    two passing tests

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,4 +1,16 @@
 class Cell
-  def neighbours_count
+  attr_accessor :x, :y
+
+  def initialize(x = 0, y = 0)
+    @x = x
+    @y = y
+  end
+
+  def neighbours
+    []
+  end
+
+  def spawn_at(x,y)
+    Cell.new(x, y)
   end
 end
--- spec/gol_spec.rb
+++ spec/gol_spec.rb
@@ -5,11 +5,20 @@ require_relative '../lib/game_of_life.rb'
 
 describe 'game of life' do
 
+  context 'cell utility methods' do
+    subject { Cell.new }
+    it "spawn relative to cell" do
+      cell = subject.spawn_at(3,5)
+      cell.is_a?(Cell).should be_true
+      cell.x.should eq(3)
+      cell.y.should eq(5)
+    end
+  end
 
   it 'should die if fewer than two life neighbours' do
     # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
     cell = Cell.new
-    cell.neighbours_count.should eq(0)
+    cell.neighbours.count.should eq(0)
   end
 # Any live cell with two or three live neighbours lives on to the next generation.
 # Any live cell with more than three live neighbours dies, as if by overcrowding.
